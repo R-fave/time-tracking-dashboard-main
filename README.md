8f96f3a8f3d057c9fae61f482dabcabdce2e26c2
R-fave <techclime1@gmail.com>
2023-11-21 14:45:32 +0100

    main

---
+++ README.md
@@ -0,0 +1,52 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+- [R-fave](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshot
+![Desktop View](desktop-view.png)
+![Mobile-View](mobile-view.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+### What I learned
+
+
+## Author
+
+- Frontend Mentor - [@R-fave](https://www.frontendmentor.io/profile/R-fave)
+
+**Note: Delete this note and add/remove/edit lines above based on what links you'd like to share.**
+
+## Acknowledgments
---
+++ javascript/index.js
@@ -0,0 +1,104 @@
+const work = document.getElementById("work")
+const play = document.getElementById("play")
+const study = document.getElementById("study")
+const exercise = document.getElementById("exercise")
+const social = document.getElementById("social")
+const selfCare = document.getElementById("self-care")
+
+import data from "./data.json" assert { type: "json" }
+
+console.log(data[0])
+
+daily()
+
+
+document.getElementById("daily").addEventListener("click", function(){
+    daily()
+})
+
+document.getElementById("weekly").addEventListener("click", function(){
+    weekly()
+})
+
+document.getElementById("monthly").addEventListener("click", function(){
+    monthly()
+})
+
+function daily(){
+    document.getElementById("cards").innerHTML =
+    data.map(datas=>{
+        return(`
+            <section id=${datas.title}>
+            <div class="back-card ${datas.title}">
+              <img src=${datas.image} >
+            </div> 
+      
+            <div class="info-card">
+              <div>
+                <h4>${datas.title}</h4>
+                <i class="fa-solid fa-ellipsis"></i>
+                
+              </div>
+      
+              <div>
+                <p>${datas.timeframes.daily.current}hrs</p>
+                <span>Last Hours - ${datas.timeframes.daily.previous}hrs</span>
+              </div>
+          </div>
+          </section>`
+        )
+    })
+}
+
+
+function weekly(){
+    document.getElementById("cards").innerHTML =
+    data.map(datas=>{
+        return(`
+            <section id=${datas.title}>
+            <div class="back-card ${datas.title}">
+              <img src=${datas.image} >
+            </div> 
+      
+            <div class="info-card">
+              <div>
+                <h4>${datas.title}</h4>
+                <i class="fa-solid fa-ellipsis"></i>
+              </div>
+      
+              <div>
+                <p>${datas.timeframes.weekly.current}hrs</p>
+                <span>Last Week - ${datas.timeframes.weekly.previous}hrs</span>
+              </div>
+          </div>
+          </section>`
+        )
+    })
+}
+
+function monthly(){
+    document.getElementById("cards").innerHTML =
+    data.map(datas=>{
+        return(`
+            <section id=${datas.title}>
+            <div class="back-card ${datas.title}">
+              <img src=${datas.image} >
+            </div> 
+      
+            <div class="info-card">
+              <div>
+                <h4>${datas.title}</h4>
+                <i class="fa-solid fa-ellipsis"></i>
+              </div>
+      
+              <div>
+                <p>${datas.timeframes.monthly.current}hrs</p>
+                <span>Last Month - ${datas.timeframes.monthly.previous}hrs</span>
+              </div>
+          </div>
+          </section>`
+        )
+    })
+}
+
+
